a8d7e0d5359525d0c6bebc6a8329f2fc688c6551
shane-cpu <59987265+shane-cpu@users.noreply.github.com>
2020-11-07 22:52:43 -0500

    Updated some more html

---
+++ docs/ExtraJS/Example.js
@@ -0,0 +1,22 @@
+
+let TDB = document.getElementById("2DB");
+TDB.addEventListener("click", function() {
+    location.replace("2DBoidExample.html")
+});
+TDB.addEventListener("mouseenter", function() {
+    document.getElementById("2DB").style.borderColor = "orange";
+});
+TDB.addEventListener("mouseleave", function() {
+    document.getElementById("2DB").style.borderColor = "white";
+});
+
+let THDB = document.getElementById("3DB");
+THDB.addEventListener("click", function() {
+    location.replace("3DBoidExample.html")
+});
+THDB.addEventListener("mouseenter", function() {
+    document.getElementById("3DB").style.borderColor = "orange";
+});
+THDB.addEventListener("mouseleave", function() {
+    document.getElementById("3DB").style.borderColor = "white";
+});
