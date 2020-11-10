35ba1cc6ee63d2e7f1acf232461b1e957009e100
shane-cpu <59987265+shane-cpu@users.noreply.github.com>
2020-11-09 21:37:59 -0500

    thing

---
+++ docs/Examples/FlowField/Sim.js
@@ -0,0 +1,6 @@
+
+// We need a particle system
+// we need a way to define a flow field
+// we need a visual representation of the vectors (arrows)
+// Look into noise algorithms
+// Look into dijkstra's algorithm to see about making some cool paths using flow fields
