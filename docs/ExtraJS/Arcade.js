ca471143bb65cb9555cc360397e9d6574e1d3282
shane-cpu <59987265+shane-cpu@users.noreply.github.com>
2022-05-05 23:04:16 -0400

    Changed a few things with the default css
    I'm going to kick someone's ass. Honestly not even sure why I can't get a list to vertically align but I've been at it for a while.

--- docs/ExtraJS/Arcade.js
+++ docs/ExtraJS/Arcade.js
@@ -3,31 +3,13 @@ let Checkers = document.getElementById("Chec");
 Checkers.addEventListener("click", function() {
     location.replace("Checkers.html")
 });
-Checkers.addEventListener("mouseenter", function() {
-    document.getElementById("Chec").style.borderColor = "orange";
-});
-Checkers.addEventListener("mouseleave", function() {
-    document.getElementById("Chec").style.borderColor = "white";
-});
 
 let Slap = document.getElementById("Slap");
 Slap.addEventListener("click", function() {
     location.replace("Slap.html")
 });
-Slap.addEventListener("mouseenter", function() {
-    document.getElementById("Slap").style.borderColor = "orange";
-});
-Slap.addEventListener("mouseleave", function() {
-    document.getElementById("Slap").style.borderColor = "white";
-});
 
 let Tank = document.getElementById("Tank");
 Tank.addEventListener("click", function() {
     location.replace("Tank.html")
-});
-Tank.addEventListener("mouseenter", function() {
-    document.getElementById("Tank").style.borderColor = "orange";
-});
-Tank.addEventListener("mouseleave", function() {
-    document.getElementById("Tank").style.borderColor = "white";
 });
